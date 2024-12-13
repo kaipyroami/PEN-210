468b2e239307a813d5ad1f3429c32196f70c3b6c
Kyle Crockett <kyle@kyle-crockett.com>
2024-12-13 12:23:38 -0800

    Add check for Kali

--- tools_setup.sh
+++ tools_setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Check if the OS is Kali Linux
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [ "$ID" != "kali" ]; then
+        echo "This script is intended to run on Kali Linux."
+        exit 1
+    fi
+else
+    echo "This script is intended to run on Kali Linux."
+    exit 1
+fi
+
 # Update package list and upgrade all packages
 sudo apt update && sudo apt upgrade -y
 
 # Install desired packages
 sudo apt install -y wireshark aircrack-ng john hashcat \
-  crunch rsmangler cowpatty bettercap kismet
+  crunch rsmangler cowpatty bettercap kismet wordlists
 
 # Clean up
 sudo apt autoremove -y
